a476f907f794eb437ad91de0f4f5626b78acba58
Dominique van de Vorle <dvdvorle@gmail.com>
2011-12-27 23:20:14 +0100

    Basic implementation of the CLI with NDesk.Options.

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NDesk.Options;
+using DvdV.Subbed.Core.Formats;
+using System.Diagnostics;
 
 namespace DvdV.Subbed.CLI
 {
@@ -9,6 +12,79 @@ namespace DvdV.Subbed.CLI
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Verbs todo = 0;
+                TimeSpan target = TimeSpan.FromSeconds(0);
+                Func<ISubtitle, bool> filter = s => true;
+                double factor = 0;
+
+                var p = new OptionSet()
+                {
+                    { "s|stretchby=",
+                        v => 
+                            { 
+                                todo = Verbs.StretchBy;
+                                factor = double.Parse(v);
+                            } },
+                    { "t|transposeby=",
+                        v =>
+                            {
+                                todo = Verbs.TransposeBy;
+                                target = TimeSpan.FromSeconds(double.Parse(v));
+                            } },
+                    { "e|extrapolate={+}",
+                        (t, v) =>
+                            {
+                                todo = Verbs.Extrapolate;
+                                filter = s => s.Text.StartsWith(t);
+                                target = TimeSpan.FromSeconds(double.Parse(v));
+                            } }
+                };
+
+                var fileNames = p.Parse(args);
+
+                if (fileNames.Count() < 1 || fileNames.Count() > 2)
+                {
+                    throw new Exception("Provide 1 or 2 filenames");
+                }
+              
+                string inputFile = fileNames[0];
+
+                bool outputSameAsInput = fileNames.Count() > 1;
+                string outputFile = outputSameAsInput ? fileNames[1] : fileNames[0];
+
+                var subs = Subbed.Core.Parsers.SubRip.Read(inputFile);
+                var man = new Subbed.Core.Formats.SubtitleManager(subs);
+
+                switch (todo)
+                {
+                    case Verbs.Extrapolate:
+                        var targetSub = subs.FirstOrDefault(filter);
+                        man.Extrapolate(targetSub, target);
+
+                        break;
+                    case Verbs.StretchBy:
+                        man.StretchBy(factor);
+
+                        break;
+                    case Verbs.TransposeBy:
+                        man.TransposeBy(target);
+
+                        break;
+                    default:
+                        Console.WriteLine("Usage: CLI.exe [Stuff]");
+                        p.WriteOptionDescriptions(Console.Out);
+                        break;
+                }
+
+                subs = man.Subtitles;
+                Subbed.Core.Parsers.SubRip.Write(subs, outputFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
---
+++ CLI/Verbs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DvdV.Subbed.CLI
+{
+    enum Verbs
+    {
+        TransposeBy = 1,
+        StretchBy = 2,
+        Extrapolate = 3
+    }
+}
